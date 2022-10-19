93d35391b96ae02b741ca4778e45bbf6ed4f37a3
d0uwhs <d0uwhs@gmail.com>
2022-10-20 01:33:56 +0900

    Init Project

---
+++ src/main/java/com/dq/devaquariumshooks/DevaquariumsHooksApplication.java
@@ -0,0 +1,13 @@
+package com.dq.devaquariumshooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevaquariumsHooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevaquariumsHooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dq/devaquariumshooks/DevaquariumsHooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dq.devaquariumshooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevaquariumsHooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
